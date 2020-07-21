26a4c0f31d9f3034581b1bf26ed0059a67b6411f
Nycor Estrada <nycor123@gmail.com>
2020-07-21 10:43:04 +0800

    Delete firebase keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDGmuonACoiII7T9v9WAuJfZ2keupaXHi0',
-    authDomain: 'esb-app-fe500.firebaseapp.com',
-    databaseURL: 'https://esb-app-fe500.firebaseio.com',
-    projectId: 'esb-app-fe500',
-    storageBucket: 'esb-app-fe500.appspot.com',
-    messagingSenderId: '323031060037',
-    appId: "1:323031060037:web:8c94b3a3c0e2524f161d2c",
-    measurementId: "G-H7GY5GYCL3"
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: "",
+    measurementId: ""
   }
 };
 
